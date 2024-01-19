817afc4aded2506d596eeda43de93aa5bc0f44a7
Omarmenjivardev11 <omarmenjivar083@gmail.com>
2024-01-19 15:49:32 -0600

    first commit

---
+++ src/main/java/com/omardev/marketapp/MarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.omardev.marketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketAppApplication.class, args);
+    }
+
+}
